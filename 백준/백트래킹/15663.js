82612f009253f57688e6486f0e98de33419a3222
cloud0406 <cloud0406@naver.com>
2023-03-01 22:52:07 +0900

    [백준 문제 풀이] 20230301
    * [15663] N과 M (9)

---
+++ 백준/백트래킹/15663.js
@@ -0,0 +1,34 @@
+const solve = (N, M, arr) => {
+  const chosen = new Array(N).fill(false);
+  const permutation = [];
+  const output = [];
+  const recursion = () => {
+    if (permutation.length === M) {
+      output.push(permutation.join(" "));
+    } else {
+      chosen.forEach((bool, i) => {
+        if (!bool) {
+          chosen[i] = true;
+          permutation.push(arr[i]);
+          recursion();
+          chosen[i] = false;
+          permutation.pop();
+        }
+      });
+    }
+  };
+  recursion();
+  console.log([...new Set(output)].join("\n"));
+};
+
+const [N, M, ...arr] = require("fs")
+  .readFileSync(__dirname + "/input/15663.txt")
+  .toString()
+  .trim()
+  .split(/\s+/);
+
+console.log(N, M, arr);
+
+arr.sort((a, b) => a - b);
+
+solve(+N, +M, arr);
