f08723dea81414ad05ba323c1c6f35179f99b55d
cloud0406 <cloud0406@naver.com>
2023-03-11 22:24:40 +0900

    [코드 수정] 20230311
    * [20438] 출석체크

--- 백준/누적합/20438.js
+++ 백준/누적합/20438.js
@@ -9,16 +9,18 @@ function solution(input) {
   /* input 정리 */
   const [N, K, Q, M] = input[0];
   const sleeps = input[1];
+  // 졸고 있는 학생은 미리 빼줌
   const students = input[2].filter((student) => !sleeps.includes(student));
 
-  /* 문제 풀이 */
   const preSum = [0, 0, 0];
 
   for (let i = 3; i <= N + 2; i++) {
     preSum[i] = preSum[i - 1];
 
+    // 졸고 있는 학생은 패스
     if (sleeps.includes(i)) continue;
 
+    // 출석 코드 받은 학생들의 배수에 해당하면 preSum에서 1증가
     for (const student of students) {
       if (i % student === 0) {
         preSum[i] += 1;
@@ -28,6 +30,7 @@ function solution(input) {
   }
 
   let answer = "";
+
   for (let i = 3; i < M + 3; i++) {
     const [S, E] = input[i];
     answer += E - S + 1 - (preSum[E] - preSum[S - 1]) + "\n";
