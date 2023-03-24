8a6709a1ef598c5d0843995a95cf810ec08e6e1f
cloud0406 <cloud0406@naver.com>
2023-03-24 13:49:45 +0900

    [백준 문제 풀이] 20230324
    * [11284] 초성 중성 종성
    * [11285] 초성 중성 종성 2

---
+++ 백준/문자열/11285.js
@@ -0,0 +1,110 @@
+const input = require("fs")
+  .readFileSync(
+    process.platform === "linux" ? "/dev/stdin" : __dirname + "/input/11285.txt"
+  )
+  .toString()
+  .trim()
+  .split("\n");
+
+let n = 44032;
+
+// 초성 19개
+first = [
+  "ㄱ",
+  "ㄲ",
+  "ㄴ",
+  "ㄷ",
+  "ㄸ",
+  "ㄹ",
+  "ㅁ",
+  "ㅂ",
+  "ㅃ",
+  "ㅅ",
+  "ㅆ",
+  "ㅇ",
+  "ㅈ",
+  "ㅉ",
+  "ㅊ",
+  "ㅋ",
+  "ㅌ",
+  "ㅍ",
+  "ㅎ",
+];
+
+// 중성 21개
+second = [
+  "ㅏ",
+  "ㅐ",
+  "ㅑ",
+  "ㅒ",
+  "ㅓ",
+  "ㅔ",
+  "ㅕ",
+  "ㅖ",
+  "ㅗ",
+  "ㅘ",
+  "ㅙ",
+  "ㅚ",
+  "ㅛ",
+  "ㅜ",
+  "ㅝ",
+  "ㅞ",
+  "ㅟ",
+  "ㅠ",
+  "ㅡ",
+  "ㅢ",
+  "ㅣ",
+];
+
+// 종성 28개
+third = [
+  "",
+  "ㄱ",
+  "ㄲ",
+  "ㄳ",
+  "ㄴ",
+  "ㄵ",
+  "ㄶ",
+  "ㄷ",
+  "ㄹ",
+  "ㄺ",
+  "ㄻ",
+  "ㄼ",
+  "ㄽ",
+  "ㄾ",
+  "ㄿ",
+  "ㅀ",
+  "ㅁ",
+  "ㅂ",
+  "ㅄ",
+  "ㅅ",
+  "ㅆ",
+  "ㅇ",
+  "ㅈ",
+  "ㅊ",
+  "ㅋ",
+  "ㅌ",
+  "ㅍ",
+  "ㅎ",
+];
+
+for (let i = 0; i < first.length; i++) {
+  if (input[0] === first[i]) {
+    n += i * 21 * 28;
+    break;
+  }
+}
+for (let i = 0; i < second.length; i++) {
+  if (input[1] === second[i]) {
+    n += i * 28;
+    break;
+  }
+}
+for (let i = 0; i < third.length; i++) {
+  if (input[2] === third[i]) {
+    n += i;
+    break;
+  }
+}
+
+console.log(String.fromCharCode(n));
