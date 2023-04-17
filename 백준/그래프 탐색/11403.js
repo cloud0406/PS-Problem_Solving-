5a47dcef704554b956e68ce8e8679c7eecf9b158
cloud0406 <cloud0406@naver.com>
2023-04-17 23:53:07 +0900

    [코드 수정] 20230417
    * [11403] 경로찾기
    * 코드 일부 수정 및 주석 추가

--- 백준/그래프 탐색/11403.js
+++ 백준/그래프 탐색/11403.js
@@ -1,6 +1,10 @@
-const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
-const input = fs.readFileSync(filePath).toString().trim().split("\n");
+const input = require("fs")
+  .readFileSync(
+    process.platform === "linux" ? "/dev/stdin" : __dirname + "/input/11403.txt"
+  )
+  .toString()
+  .trim()
+  .split("\n");
 
 let N = +input.shift();
 let arr = [];
@@ -9,16 +13,20 @@ for (const row of input) {
   arr.push(row.split(" ").map(Number));
 }
 
-for (let k = 0; k < N; k++) {
-  for (let i = 0; i < N; i++) {
-    for (let j = 0; j < N; j++) {
-      if (arr[i][k] && arr[k][j]) {
-        arr[i][j] = 1;
+// 플로이드 와샬
+function solution(arr, N) {
+  for (let k = 0; k < N; k++) {
+    for (let from = 0; from < N; from++) {
+      for (let to = 0; to < N; to++) {
+        // k를 통해서 i -> j 이동이 가능하다면
+        if (arr[from][k] && arr[k][to]) arr[from][to] = 1;
       }
     }
   }
+
+  for (let i = 0; i < N; i++) {
+    console.log(arr[i].join(" "));
+  }
 }
 
-for (let i = 0; i < N; i++) {
-  console.log(arr[i].join(" "));
-}
+solution(arr, N);
