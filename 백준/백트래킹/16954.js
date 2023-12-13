cd80ef532aadf5540670143f0735dc2083ad336a
cloud0406 <cloud0406@naver.com>
2023-12-13 20:48:22 +0900

    [백준 문제 풀이] 20231213

--- 백준/백트래킹/16954.js
+++ 백준/백트래킹/16954.js
@@ -1,19 +1,29 @@
+const input = require("fs")
+  .readFileSync(
+    process.platform === "linux" ? "/dev/stdin" : __dirname + "/input/16954.txt"
+  )
+  .toString()
+  .trim()
+  .split("\n");
+
 const solutionRecursion = (board) => {
   const START = [7, 0];
   const EMPTY = ".";
   const TOP_LINE = 0;
   const NEWROW = ["........"];
 
+  // 백트래킹으로 추적
   const bt = (cRow, cCol, currBoard, top) => {
     if (cRow === 0 && cCol === 7) return 1;
 
     if (!currBoard[cRow] || currBoard[cRow][cCol] !== EMPTY) return 0;
 
-    if (cRow === 0) return 1;
-    if (cRow <= top) return 1;
+    if (cRow === 0 || cRow <= top) return 1;
 
+    // 다음 라운드, 보드 한 줄씩 아래로 이동
     const nextBoard = NEWROW.concat(currBoard.slice(0, -1));
 
+    // 캐릭터 이동할 수 있는 모든 경우의 수
     const ways = [
       [-1, 0],
       [0, 1],
@@ -25,10 +35,12 @@ const solutionRecursion = (board) => {
       [1, -1],
       [1, 1],
     ];
+
     for (const [nr, nc] of ways) {
       const nRow = cRow + nr;
       const nCol = cCol + nc;
 
+      // 다음 좌표가 정상이고, 벽이 아니고, 다음 좌표에서 재귀적으로 탐색 가능한 경우
       if (
         currBoard[nRow] &&
         currBoard[nRow][nCol] === EMPTY &&
@@ -37,80 +49,13 @@ const solutionRecursion = (board) => {
         return 1;
     }
 
+    // 위에서 탐색 불가능한 경우 return false
     return 0;
   };
 
+  // 출발점에서 백트래킹 시작
   let [sRow, sCol] = START;
   return bt(sRow, sCol, board, TOP_LINE);
 };
 
-const solutionIteration = (board) => {
-  const START = [7, 0];
-  const BYUK = "#";
-  const NEWROW = ["........"];
-  let top = -1;
-
-  // 가장 높은 벽의 위치 찾기
-  for (const row in board) {
-    if (board[row].includes(BYUK)) {
-      top = row;
-      break;
-    }
-  }
-
-  if (top === -1) return 1;
-
-  const ways = [
-    [-1, 0],
-    [0, 1],
-    [0, -1],
-    [-1, -1],
-    [-1, 1],
-    [0, 0],
-    [1, 0],
-    [1, -1],
-    [1, 1],
-  ];
-  const queue = [START];
-
-  while (queue.length) {
-    let thisLoop = queue.length;
-    while (thisLoop--) {
-      const [crow, ccol] = queue.shift();
-
-      if (crow < 0 || crow > 7 || ccol < 0 || ccol > 7) continue;
-      if (board[crow][ccol] === BYUK) continue;
-
-      if (crow < top || (crow === 0 && ccol === 7)) return 1;
-
-      for (const [row, col] of ways) {
-        const nrow = crow + row;
-        const ncol = ccol + col;
-        if (nrow < 0 || nrow > 7 || ncol < 0 || ncol > 7) continue;
-        if (board[nrow][ncol] === BYUK) continue;
-        queue.push([nrow, ncol]);
-      }
-    }
-
-    board = NEWROW.concat(board.slice(0, -1));
-
-    top++;
-  }
-
-  return 0;
-};
-
-const readline = require("readline");
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  prompt: "",
-});
-
-let q = [];
-rl.on("line", (line) => {
-  line ? q.push(line) : rl.close();
-}).on("close", () => {
-  console.log(solution(q));
-  process.exit();
-});
+console.log(solutionRecursion(input));
