244e234e0cc8acccbabeeb8dc87444ec5115db00
cloud0406 <cloud0406@naver.com>
2024-06-10 16:38:15 +0900

    [백준 다시 풀기] 20240610

--- 백준/완전탐색/21278.js
+++ 백준/완전탐색/21278.js
@@ -10,8 +10,6 @@ const [N, M] = input[0].split(" ").map(Number);
 const graph = input.slice(1).map((v) => v.split(" ").map(Number));
 
 function solution(N, M, graph) {
-  let answer = Infinity;
-  let answerArr = [];
   const dist = Array.from({ length: N + 1 }, () => Array(N + 1).fill(Infinity));
 
   for (let [from, to] of graph) {
@@ -19,9 +17,7 @@ function solution(N, M, graph) {
     dist[to][from] = 1;
   }
 
-  dist.forEach((_, idx) => {
-    dist[idx][idx] = 0;
-  });
+  dist.forEach((_, idx) => (dist[idx][idx] = 0));
 
   for (let i = 1; i <= N; i++) {
     for (let j = 1; j <= N; j++) {
@@ -33,21 +29,25 @@ function solution(N, M, graph) {
     }
   }
 
+  let answer = Infinity;
+  let answers = [];
+
   for (let first = 1; first <= N; first++) {
     for (let second = first + 1; second <= N; second++) {
       let time = 0;
-      for (let currentCity = 1; currentCity <= N; currentCity++) {
-        time +=
-          Math.min(dist[first][currentCity], dist[second][currentCity]) * 2;
+
+      for (let current = 1; current <= N; current++) {
+        time += Math.min(dist[first][current], dist[second][current]) * 2;
       }
-      if (time < answer) {
+
+      if (answer > time) {
         answer = time;
-        answerArr = [first, second, answer];
+        answers = [first, second, answer];
       }
     }
   }
 
-  return answerArr.join(" ");
+  return answers.join(" ");
 }
 
 console.log(solution(N, M, graph));
